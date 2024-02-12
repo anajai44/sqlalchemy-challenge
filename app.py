01c4a5624354ff1e05c90f65bf97043cc67193d3
ANAHI <anahi.jaimes2020@gmail.com>
2024-02-12 16:47:12 -0600

    files

--- app.py
+++ app.py
@@ -1,30 +1,103 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
+# Base.prepare(autoload_with=engine)
+Base.prepare(engine, reflect=True)
 
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+# session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+def date_prev_year():
+    # Create the session
+    session = Session(engine)
+
+    most_recent_date = session.query(func.max(Measurement.date)).first()[0]
+    first_date = dt.datetime.strptime(most_recent_date, "%Y-%m-%d") - dt.timedelta(days=365)
+
+    session.close()
+
+    return(first_date)
+
+@app.route("/")
+def homepage():
+    return """ <h1> Welcome to Honolulu, Hawaii Climate API! </h1>
+    <h3> The available routes are: </h3>
+    <ul>
+    <li><a href = "/api/v1.0/precipitation"> Precipitation</a>: <strong>/api/v1.0/precipitation</strong> </li>
+    <li><a href = "/api/v1.0/stations"> Stations </a>: <strong>/api/v1.0/stations</strong></li>
+    <li><a href = "/api/v1.0/tobs"> TOBS </a>: <strong>/api/v1.0/tobs</strong></li>
+"""
+    
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    prcp_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= date_prev_year()).all()
+    session.close()
+
+    prcp_list = []
+    for date, prcp in prcp_data:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        prcp_list.append(prcp_dict)
+    return jsonify(prcp_list)
+
+    
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    station_data = session.query(Station.station).all()
+    session.close()
+
+    station_list = list(np.ravel(station_data))
+    return jsonify(station_list)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    tobs_data = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == 'USC00519281').\
+                        filter(Measurement.date >= date_prev_year()).all()
+    session.close()
+
+    tobs_list = []
+    for date, tobs in tobs_data:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        tobs_list.append(tobs_dict)
+    return jsonify(tobs_list)
+
+
+   
+if __name__ == '__main__':
+    app.run(debug=True) 
